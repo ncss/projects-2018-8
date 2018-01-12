6593b345a52ce335e83670a86c6cd477210fe478
Mitchell-Torok <mitch.torok@gmail.com>
2018-01-12 11:05:35 +1100

    added comments

--- Team 4/Watch_Code/Shake_accel_speed1.py
+++ Team 4/Watch_Code/Shake_accel_speed1.py
@@ -1,14 +1,23 @@
+# Import Modules
 from microbit import *
 import radio
+# Set Radio
 radio.on()
 radio.config(channel = 41)
+# Set Variables
 prev_time = 0;
 time = 0;
+# Check Repl
 print("FISH")
+# Loop Program
 while True:
-  if accelerometer.was_gesture("shake"):
-    time = running_time() - prevTime
-    radio.send(str(time))
-    print(time)
-    prevTime = running_time()  
+    # Read Gesture
+    if accelerometer.was_gesture("shake"):
+        # Set Time Between Gestures
+        time = running_time() - prev_time
+        # Display And Send Time
+        print(time)
+        radio.send(str(time))
+        # Reset Time 
+        prev_time = running_time()  
 
