1c559df46d01f322ad05836f8776aaa84257f816
BoomCheese8 <scott.rose888@gmail.com>
2018-01-12 15:45:58 +1100

    Cleaned & Added game radio checks

--- Team 3/Master.py
+++ Team 3/Master.py
@@ -7,30 +7,28 @@ POINTS = 50000     # The amount of points (in ms) the player begins with.
 radio.on()
 radio.config(channel = GAME_CHANNEL)
 answer = None
-decrease_points = False
+question_asked = False
 while True:
     # Initialises timer & sets correct answer.
     if button_a.was_pressed():
         answer = "true"
         start_time = running_time()
-        decrease_points = True
+        question_asked = True
     if button_b.was_pressed():
         answer = "false"
         start_time = running_time()
-        decrease_points = True
+        question_asked = True
     # Checks for incoming answer, checks if answer is correct, subtracts points based on time elapsed and INCORRECT_PENALTY.
-    incoming = radio.receive()
-    if incoming:
-        if incoming == answer:
-            radio.send(answer)
-            decrease_points = False
-        if incoming != answer:
-            POINTS -= INCORRECT_PENALTY
-            radio.send(answer)
-            decrease_points = False
-    if decrease_points:
-        end_time = running_time()
-        elapsed_time = end_time - start_time
-        start_time = end_time
-        POINTS -= elapsed_time
-        print(POINTS)
+    if question_asked:
+        incoming = radio.receive()
+        if incoming and incoming.startswith("tf:"):
+            if incoming != answer:
+                POINTS -= INCORRECT_PENALTY
+            question_asked = False
+            radio.send("tf:" + answer)
+        if question_asked:
+            end_time = running_time()
+            elapsed_time = end_time - start_time
+            start_time = end_time
+            POINTS -= elapsed_time
+            print(POINTS)
