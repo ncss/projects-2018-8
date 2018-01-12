de764aa8cb1eae582e82e64b11af160ce9275321
TimothyHorscroft <1006818@schoolsnet.act.edu.au>
2018-01-12 16:57:24 +1100

    updated master
    yay

--- Team 3/Master.py
+++ Team 3/Master.py
@@ -1,6 +1,3 @@
-# Things to add:
-# what happens when POINTS <= 0 (remember the less than because you are potentially subtracting more than 1 each tick)
-
 from microbit import *
 import radio
 # MASTER
@@ -10,6 +7,10 @@ POINTS = 50000     # The amount of points (in ms) the player begins with.
 radio.on()
 radio.config(channel = GAME_CHANNEL)
 question_asked = False
+questions_answered = 0
+
+display.scroll("start", wait=False)
+
 while True:
     # Initialises timer & sets correct answer.
     if button_a.was_pressed():
@@ -21,16 +22,24 @@ while True:
         start_time = running_time()
         question_asked = True
     # Checks for incoming answer, checks if answer is correct, subtracts points based on time elapsed and INCORRECT_PENALTY.
-    print(question_asked, POINTS)
+    incoming = radio.receive()
     if question_asked:
-        incoming = radio.receive()
-        if incoming and incoming.startswith("tf:"):
-            if incoming != "tf:" + answer:
+        if incoming and incoming.startswith("tof:"):
+            if incoming == "tof:" + answer:
+                questions_answered += 1
+            else:
                 POINTS -= INCORRECT_PENALTY
             question_asked = False
-            radio.send("tf:" + answer)
-        if question_asked:
+            display.scroll("Score: {}".format(POINTS), wait=False)
+            radio.send("tof:" + answer)
+        else:
             end_time = running_time()
             elapsed_time = end_time - start_time
             start_time = end_time
             POINTS -= elapsed_time
+    
+    if POINTS <= 0:
+        display.scroll("Game over! {} questions answered!".format(questions_answered))
+        questions_answered = 0
+        POINTS = 50000
+        question_asked = False
