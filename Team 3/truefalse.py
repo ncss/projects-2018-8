50a9277126a1d78011fcbc034fd2d741726b144a
BoomCheese8 <scott.rose888@gmail.com>
2018-01-12 17:27:30 +1100

    Added display and sound for correct/incorrect answers

--- Team 3/truefalse.py
+++ Team 3/truefalse.py
@@ -5,7 +5,8 @@ GAME_CHANNEL = 49
 radio.config(channel=GAME_CHANNEL)
 radio.on()
 
-
+TICK = Image("00000:00009:00090:90900:09000")
+CROSS = Image("90009:09090:00900:09090:90009")
 class Button:
     def __init__(self, pin, boolean):
         self.pin = pin
@@ -22,17 +23,33 @@ class Button:
         
         return False
 
-
-# button = Button(pin0, True)
-button = button_a
+button = Button(pin0, True)
+CELEBRATE_TIME = 3000
+celebrate_timer = CELEBRATE_TIME
+previous_time = running_time()
 
 while True:
     msg = radio.receive()
-    if msg:
-        print(msg)
+    if msg and msg.startswith("tof:"):
+        if (button.boolean and msg == "tof:true") or (not button.boolean and msg == "tof:false"):
+            display.show(TICK)
+            music.play(music.POWER_UP)
+            celebrate_timer = CELEBRATE_TIME
+        else:
+            display.show(CROSS)
+            music.play(music.WAWAWAWAA)
+            celebrate_timer = CELEBRATE_TIME
 
     if button.was_pressed():
-        if True:
+        if button.boolean:
             radio.send("tof:true")
         else:
             radio.send("tof:false")
+    
+    current_time = running_time()
+    time_elapsed = current_time - previous_time
+    previous_time = current_time
+    celebrate_timer -= time_elapsed
+    if celebrate_timer <= 0:
+        celebrate_timer = 0
+        display.clear()
