702bd87a7f96c9dee1224a78d4f66da1fd548219
Jack Jiang <jackywathy24@gmail.com>
2018-01-13 14:18:03 +1100

    tay code

--- Team 1/bitbot_tay.py
+++ Team 1/bitbot_tay.py
@@ -1,39 +1,46 @@
 from microbit import*
 import radio
 radio.on()
-radio.config(channel = 51)
+radio.config(channel = 51, address=0x77696c6c)
+start_speed = 300    
 
 lefty_speed = pin0
 lefty_dir = pin8
 righty_speed = pin1
 righty_dir = pin12
-turn_sleep = 210
+turn_sleep = 400
+current_speed = start_speed
+speed_change = 100
+
 
 def left():
-    lefty_speed.write_digital(0)
+    lefty_speed.write_analog(0)
     lefty_dir.write_digital(0)
-    righty_speed.write_digital(1)
+    righty_speed.write_analog(1023)
     righty_dir.write_digital(0)
     sleep(turn_sleep)
+    run_motor()
     
 def right():
-    lefty_speed.write_digital(1)
+    lefty_speed.write_analog(1023)
     lefty_dir.write_digital(0)
-    righty_speed.write_digital(0)
+    righty_speed.write_analog(0)
     righty_dir.write_digital(0)
-    sleep(turn_sleep)
+    sleep(turn_sleep+40)
+    run_motor()
     
 def HECK_STOP():
-    righty_speed.write_digital(0)
+    righty_speed.write_analog(0)
     righty_dir.write_digital(0)
-    lefty_speed.write_digital(0)
+    lefty_speed.write_analog(0)
     lefty_dir.write_digital(0)
     
 def forward():
-    righty_speed.write_analog(1000)
+    righty_speed.write_analog(current_speed)
     righty_dir.write_digital(0)
-    lefty_speed.write_analog(1000)
+    lefty_speed.write_analog(current_speed+7)
     lefty_dir.write_digital(0)
+    
     
 def run_motor(left=0, right=0):
     if left < 0 or right < 0 or left >= 1024 or right >= 1024:
@@ -44,29 +51,43 @@ def run_motor(left=0, right=0):
     righty_speed.write_analog(right)
     righty_dir.write_digital(0)
 
-#while True:
-    #run_motor(left=1023, right=1023)
-
-'''
-while True:
-    sleep(5000)
-    right()
-    HECK_STOP()
-    break
-'''
+stop = True
 while True:
     msg = radio.receive()
     if msg == 'G':
         forward()
+        stop = False
+        current_speed = start_speed
+            
     if msg == 'S':
         HECK_STOP()
-    if msg == '<':
-        left()
+        stop = True
+        current_speed = start_speed
+            
+    if stop:
+        # the robit is in stop mode
+        display.show(Image.SAD)
+        
+    else:
+    
+        display.show(Image.HAPPY)
+        
+        if msg == '<':
+            left()
+            forward()
+            print('L')
+        if msg == '>':
+            right()
+            forward()
+            print('R')
+        if msg == '-':
+            current_speed = max(current_speed - speed_change, 0)
+            forward()
+        if msg == '+':
+            current_speed = min(current_speed + speed_change, 1023)
         forward()
-        print('L')
-    if msg == '>':
-        right()
-        forward()
-        print('R')
+    
+    
+    
     
     
--- Team 1/quokka/main.py
+++ Team 1/quokka/main.py
@@ -1,39 +1,33 @@
 # README!
 # scrolls stuff on the microbit when recieved on channel 51
+'''
 from quokka import *
 neopixels.all(0,0,0)
 neopixels.show()
+'''
+from microbit import *
+import radio
 
-radio.config(channel=51)
-queue = []
-clear_time = 0
-def queue_text(char):
-	queue.append(char)
+
+radio.on()
+radio.config(channel=51,address=0x77696c6c)
+
 	
 while True:
     msg = radio.receive()
-    now = running_time()
-    if clear_time < now:
-        display.fill(0)
-        display.show()
-    '''
-    if msg:
-        display.text(msg, 0,0, 1)
-        display.show()
-        sleep(1000)
-    '''
-    if buttons.a.was_pressed():
+    display.show("@")
+   
+   
+    if button_a.was_pressed():
         radio.send("G")
-        display.text("GO", 0, 0, 1)
-        display.show()
+        display.scroll("GO", wait=False)
         print("GOHI!!")
         sleep(1000)
 	
 	# button b is hooked up to D for some reason
-    if buttons.d.was_pressed():
+    if button_b.was_pressed():
         radio.send("S")
-        display.text("STOP", 0, 0, 1)
-        display.show()
+        display.scroll("STOP", wait=False)
         print("HI!")
         sleep(1000)
     
