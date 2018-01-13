cd51fb924150bbb3cf4cacf7400d3a41715555d3
Jack Jiang <jackywathy24@gmail.com>
2018-01-13 14:17:27 +1100

    Auto stash before merge of "master" and "origin/master"

---
+++ Team 1/kill_switch.py
@@ -0,0 +1,37 @@
+# README!
+# scrolls stuff on the microbit when recieved on channel 51
+'''
+from quokka import *
+neopixels.all(0,0,0)
+neopixels.show()
+'''
+from microbit import *
+import radio
+
+
+radio.on()
+radio.config(channel=51,address=0x77696c6c)
+
+	
+while True:
+    msg = radio.receive()
+    display.show("@")
+   
+   
+    if button_a.was_pressed():
+        radio.send("G")
+        display.scroll("GO", wait=False)
+        print("GOHI!!")
+        sleep(1000)
+	
+	# button b is hooked up to D for some reason
+    if button_b.was_pressed():
+        radio.send("S")
+        display.scroll("STOP", wait=False)
+        print("HI!")
+        sleep(1000)
+    
+		
+		
+        
+    
