c966460f2d6f5fede45e6de644a48ffd4ab42c0a
emibr <emi77brennan@gmail.com>
2018-01-13 21:33:46 +1100

    final_version

--- Team 3/neopixel.py
+++ Team 3/neopixel.py
@@ -5,26 +5,48 @@ import radio
 import math
 
 # Setup the Neopixel strip on pin0 with a length of 10 pixels
-np = neopixel.NeoPixel(pin0, 10)
+max_no_lights = 30
+np = neopixel.NeoPixel(pin13, max_no_lights)
 
-max_no_lights = 10
+
 lights_on = max_no_lights
 max_value = 60000
+
 radio.on()
 radio.config(channel = 49)
-can_i_loop = True
 msg = ""
 
+for pixel_id in range(0, max_no_lights):
+    np[pixel_id] = (0, 100, 0)
+
+np.show()
+
 while True:
+    msg = radio.receive()
     if msg and msg.startswith("score:"):
+        print(msg)
         new_msg = msg[6:]
         new_no = int(new_msg)
         lights_on = math.ceil(new_no/max_value*max_no_lights)
-        lights_off = max_no_lights - lights_on
+        print(lights_on)
         for pixel_id in range(lights_on, max_no_lights):
             np[pixel_id] = (0, 0, 0)
-        can_i_loop = True
+        np.show()
+        if lights_on <= max_no_lights/2:
+            for pixel_id in range(0, lights_on):
+                np[pixel_id] = (127, 60, 3)
+            np.show()
+        if lights_on <= max_no_lights/5:
+            for pixel_id in range(0, lights_on):
+                np[pixel_id] = (100, 0, 0)
+            np.show()
 
+
+
+
+
+
+'''
     if can_i_loop:
         for thisloop in range(0,100):
             for pixel_id in range(0, lights_on):
@@ -68,3 +90,5 @@ while True:
                 can_i_loop = False
                 break
             sleep(10)
+'''
+
