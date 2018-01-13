327fd78de5a4e29b61dedbb399411edd0c0505fe
Jack Jiang <jackywathy24@gmail.com>
2018-01-13 21:19:43 +1100

    Auto stash before merge of "master" and "origin/master"

--- Team 1/bitbot_tay.py
+++ Team 1/bitbot_tay.py
@@ -6,6 +6,8 @@ import random
 radio.on()
 radio.config(channel = 51, address=0x77696c6c)
 
+    
+
 start_speed = 200    
 
 lefty_speed = pin0
@@ -17,13 +19,17 @@ current_speed = start_speed
 speed_change = 100
 pixels = NeoPixel(pin13, 12)
 
+for i in range(12):
+    pixels[i] = (0,0,0)
+pixels.show()
+
 def left():
     lefty_speed.write_analog(0)
     lefty_dir.write_digital(0)
     righty_speed.write_analog(1023)
     righty_dir.write_digital(0)
     sleep(turn_sleep)
-    run_motor()
+    HECK_STOP()
     
 def right():
     lefty_speed.write_analog(1023)
@@ -41,35 +47,24 @@ def HECK_STOP():
     
     
 def forward():
-    run_motor_forward(left=current_speed+7,right=current_speed)
+    lefty_speed.write_analog(current_speed)
+    lefty_dir.write_digital(0)
+    righty_speed.write_analog(current_speed+7)
+    righty_dir.write_digital(0)
 
 def backwards():
-    run_motor_back(left=200,right=200)
+    lefty_speed.write_analog(200)
+    lefty_dir.write_digital(1)
+    righty_speed.write_analog(200)
+    righty_dir.write_digital(1)
     sleep(turn_sleep)
     HECK_STOP()
     
     
-def run_motor_forward(left=0, right=0):
-    if left < 0 or right < 0 or left >= 1024 or right >= 1024:
-        raise Exception()
-        
-    lefty_speed.write_analog(left)
-    lefty_dir.write_digital(0)
-    righty_speed.write_analog(right)
-    righty_dir.write_digital(0)
-
-def run_motor_back(left=0, right=0):
-    if left < 0 or right < 0 or left >= 1024 or right >= 1024:
-        raise Exception()
-        
-    lefty_speed.write_analog(left)
-    lefty_dir.write_digital(1)
-    righty_speed.write_analog(right)
-    righty_dir.write_digital(1)
     
 def get_color():
     # returns (0,0,0)
-    return [random.randrange(80) for i in range(3)]
+    return [random.randrange(10) for i in range(3)]
     
 colors = [get_color() for i in range(12)]
 
@@ -78,13 +73,16 @@ def rainbow():
     for i, color in enumerate(colors):
         pixels[i] = tuple(color)
     pixels.show()
-    colors.pop(0)
-    colors.append(get_color())
+   
+    colors.append(colors.pop(0))
     
     
     
 before = 0
 change_light_time = 300
+
+
+current_round_time = 0
 
 rainbow()
 stop = True
@@ -94,8 +92,8 @@ while True:
         rainbow()
         before = running_time()
     
-    left = pin11.read_digital()
-    right = pin5.read_digital()
+    left_line = pin11.read_digital()
+    right_line = pin5.read_digital()
     msg = radio.receive()
     if msg:
         print('Receive message:', msg)
@@ -115,9 +113,8 @@ while True:
         # the robit is in stop mode
         display.show(Image.SAD)
         
-    else:
-        
-        if not left or not right:
+    else: 
+        if not left_line or not right_line:
             print("Entered!")
             display.show(Image.SWORD)
             backwards()
